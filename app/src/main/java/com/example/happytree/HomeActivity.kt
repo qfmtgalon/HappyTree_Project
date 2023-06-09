306836ce5ec3a3e505f1fda23f46a0431fedf122
qfmtgalon <faithmarygalon.fmg@gmail.com>
2023-06-09 19:37:45 +0800

    Some changes - Before removing action bar

--- app/src/main/java/com/example/happytree/HomeActivity.kt
+++ app/src/main/java/com/example/happytree/HomeActivity.kt
@@ -1,19 +1,24 @@
 package com.example.happytree
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.example.happytree.LandingPage
+import com.example.happytree.R
 import com.example.happytree.databinding.ActivityHomeBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var drawerLayout: DrawerLayout
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +29,24 @@ class HomeActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-        NavigationUI.setupWithNavController(binding.navView,navController)
-        NavigationUI.setupWithNavController(binding.navBottom,navController)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupWithNavController(binding.navBottom, navController)
+
+        val textDate = binding.homeLayout.findViewById<TextView>(R.id.textDate)
+
+        // Set current date and day
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+        val dateString = dateFormat.format(calendar.time)
+        val dayFormat = SimpleDateFormat("EEEE", Locale.getDefault())
+        val dayString = dayFormat.format(calendar.time)
+
+        textDate.text = "$dateString ($dayString)"
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHost)
-        return NavigationUI.navigateUp(navController,drawerLayout)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
     fun logout(menuItem: MenuItem) {
@@ -38,10 +54,3 @@ class HomeActivity : AppCompatActivity() {
         startActivity(intent)
     }
 }
-
-
-
-
-
-
-
