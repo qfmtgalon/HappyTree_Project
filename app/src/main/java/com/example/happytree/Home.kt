841b3b89bae972873091bd1f74f313736601189f
qfmtgalon <faithmarygalon.fmg@gmail.com>
2023-06-26 23:09:31 +0800

    Reset. No Farm Database yet.

--- app/src/main/java/com/example/happytree/Home.kt
+++ app/src/main/java/com/example/happytree/Home.kt
@@ -1,21 +1,45 @@
 package com.example.happytree
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class Home : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val currentDate = view.findViewById<TextView>(R.id.textDate)
+        val dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+        val date = Date()
+        val formattedDate = dateFormat.format(date)
+        currentDate.text = formattedDate
+
+//        val btnAdd = view.findViewById<MaterialCardView>(R.id.btnAdd)
+//        btnAdd.setOnClickListener {
+//            val navController = Navigation.findNavController(view)
+//            navController.navigate(R.id.inputFragment)
+//        }
+//
+//        val btnViewFarm = view.findViewById<MaterialCardView>(R.id.btnViewFarm)
+//        btnViewFarm.setOnClickListener{
+//            val navController = Navigation.findNavController(view)
+//            navController.navigate(R.id.viewFragment)
+//        }
+    }}
--- app/src/main/java/com/example/happytree/HomeActivity.kt
+++ app/src/main/java/com/example/happytree/HomeActivity.kt
@@ -1,20 +1,15 @@
 package com.example.happytree
 
-
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import com.example.happytree.LandingPage
-import com.example.happytree.R
 import com.example.happytree.databinding.ActivityHomeBinding
-import java.text.SimpleDateFormat
-import java.util.*
+
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -25,23 +20,14 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         drawerLayout = binding.drawerLayout
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHost) as NavHostFragment
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.myNavHost) as NavHostFragment
         val navController = navHostFragment.navController
 
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView, navController)
         NavigationUI.setupWithNavController(binding.navBottom, navController)
-
-        val textDate = binding.homeLayout.findViewById<TextView>(R.id.textDate)
-
-        // Set current date and day
-        val calendar = Calendar.getInstance()
-        val dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
-        val dateString = dateFormat.format(calendar.time)
-        val dayFormat = SimpleDateFormat("EEEE", Locale.getDefault())
-        val dayString = dayFormat.format(calendar.time)
-
-        textDate.text = "$dateString ($dayString)"
     }
 
     override fun onSupportNavigateUp(): Boolean {
