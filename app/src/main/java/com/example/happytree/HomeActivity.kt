45a1626162a4c8b0ef7f25383b1004c4016f30e8
qfmtgalon <faithmarygalon.fmg@gmail.com>
2023-06-30 17:52:13 +0800

    Woking Navigation

--- app/src/main/java/com/example/happytree/HomeActivity.kt
+++ app/src/main/java/com/example/happytree/HomeActivity.kt
@@ -27,12 +27,72 @@ class HomeActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.myNavHost) as NavHostFragment
         val navController = navHostFragment.navController
 
+        binding.navBottom.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.viewFragment -> {
+                    navController.navigate(R.id.viewFragment)
+                    true
+                }
+                R.id.home -> {
+                    navController.navigate(R.id.home)
+                    true
+                }
+//                R.id.careFragment -> {
+//                    navController.navigate(R.id.careFragment)
+//                    true
+//                }
+                else -> false
+            }
+        }
+
+        binding.navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    navController.navigate(R.id.home)
+                    true
+                }
+                R.id.viewFragment -> {
+                    navController.navigate(R.id.viewFragment)
+                    true
+                }
+                R.id.treeDiseases -> {
+                    navController.navigate(R.id.treeDiseases)
+                    true
+                }
+                R.id.about -> {
+                    navController.navigate(R.id.about)
+                    true
+                }
+                R.id.FAQs -> {
+                    navController.navigate(R.id.FAQs)
+                    true
+                }
+                else -> false
+            }
+        }
+
+
 
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView, navController)
         NavigationUI.setupWithNavController(binding.navBottom, navController)
     }
 
+
+    override fun onBackPressed() {
+        val navController = findNavController(R.id.myNavHost)
+        val currentDestination = navController.currentDestination?.id
+
+        // Check if the current destination is the WelcomeFragment
+        if (currentDestination == R.id.welcome) {
+            // Exit the app and go to the Home screen
+            moveTaskToBack(true)
+        } else if (supportFragmentManager.backStackEntryCount > 0) {
+            super.onBackPressed()
+        } else {
+            moveTaskToBack(true)
+        }
+    }
 
 
     override fun onSupportNavigateUp(): Boolean {
@@ -43,5 +103,7 @@ class HomeActivity : AppCompatActivity() {
     fun logout(menuItem: MenuItem) {
         val intent = Intent(this, LandingPage::class.java)
         startActivity(intent)
+        finish()
     }
+
 }
