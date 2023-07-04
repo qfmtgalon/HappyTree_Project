17bb0b3ed2c6ed18d92b25eb89a25be700baa107
qfmtgalon <faithmarygalon.fmg@gmail.com>
2023-07-04 18:22:34 +0800

    LATEST - Working Navigation

--- app/src/main/java/com/example/happytree/HomeActivity.kt
+++ app/src/main/java/com/example/happytree/HomeActivity.kt
@@ -1,81 +1,81 @@
 package com.example.happytree
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.example.happytree.LandingPage
+import com.example.happytree.R
 import com.example.happytree.databinding.ActivityHomeBinding
-
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
 
-
-    private lateinit var binding: ActivityHomeBinding
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        drawerLayout = binding.drawerLayout
+        setContentView(R.layout.activity_home)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.myNavHost) as NavHostFragment
+        val drawerLayout: DrawerLayout = findViewById(R.id.homeActivityLayout)
+        val navView: NavigationView = findViewById(R.id.navView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHost) as NavHostFragment
         val navController = navHostFragment.navController
 
-        binding.navBottom.setOnNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.viewFragment -> {
-                    navController.navigate(R.id.viewFragment)
-                    true
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> navController.navigate(R.id.home)
+                R.id.viewFragment -> navController.navigate(R.id.viewFragment)
+                R.id.treeDiseases -> navController.navigate(R.id.treeDiseases)
+                R.id.about -> navController.navigate(R.id.about)
+                R.id.FAQs -> navController.navigate(R.id.FAQs)
+                R.id.nav_logout -> {
+                    // Clear the session and go back to WelcomeFragment
+                    val landingPageIntent = Intent(this, LandingPage::class.java)
+                    landingPageIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(landingPageIntent)
+                    finish()
                 }
-                R.id.home -> {
-                    navController.navigate(R.id.home)
-                    true
-                }
-//                R.id.careFragment -> {
-//                    navController.navigate(R.id.careFragment)
-//                    true
-//                }
-                else -> false
             }
+            true
         }
 
-        binding.navView.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.home -> {
-                    navController.navigate(R.id.home)
-                    true
-                }
-                R.id.viewFragment -> {
-                    navController.navigate(R.id.viewFragment)
-                    true
-                }
-                R.id.treeDiseases -> {
-                    navController.navigate(R.id.treeDiseases)
-                    true
-                }
-                R.id.about -> {
-                    navController.navigate(R.id.about)
-                    true
-                }
-                R.id.FAQs -> {
-                    navController.navigate(R.id.FAQs)
-                    true
-                }
-                else -> false
+        bottomNavView = findViewById(R.id.bottomNavView)
+        bottomNavView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> navController.navigate(R.id.home)
+                R.id.viewFragment -> navController.navigate(R.id.viewFragment)
+                R.id.careFragment -> navController.navigate(R.id.careFragment)
             }
+            true
         }
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
-
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-        NavigationUI.setupWithNavController(binding.navView, navController)
-        NavigationUI.setupWithNavController(binding.navBottom, navController)
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHost)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
 
@@ -83,27 +83,17 @@ class HomeActivity : AppCompatActivity() {
         val navController = findNavController(R.id.myNavHost)
         val currentDestination = navController.currentDestination?.id
 
-        // Check if the current destination is the WelcomeFragment
-        if (currentDestination == R.id.welcome) {
+        // Check if the current destination is either updateFragment or inputFragment
+        if (currentDestination == R.id.updateFragment || currentDestination == R.id.inputFragment) {
+            // Navigate to the viewFragment
+            navController.navigate(R.id.viewFragment)
+        } else if (currentDestination != null && currentDestination != R.id.home) {
+            // Navigate back to the HomeFragment
+            navController.popBackStack(R.id.home, false)
+        } else {
             // Exit the app and go to the Home screen
             moveTaskToBack(true)
-        } else if (supportFragmentManager.backStackEntryCount > 0) {
-            super.onBackPressed()
-        } else {
-            moveTaskToBack(true)
         }
-    }
-
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHost)
-        return NavigationUI.navigateUp(navController, drawerLayout)
-    }
-
-    fun logout(menuItem: MenuItem) {
-        val intent = Intent(this, LandingPage::class.java)
-        startActivity(intent)
-        finish()
     }
 
 }
